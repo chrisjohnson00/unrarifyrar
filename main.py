ae3f5f97d78da4feb8b76d425104df6fcf1b90d7
Chris Johnson <chrisjohnson00@hotmail.com>
2022-06-14 13:04:22 -0700

    feat: including radarr

--- main.py
+++ main.py
@@ -12,10 +12,20 @@ logger = gogo.Gogo('struct', low_formatter=formatter).get_logger(**kwargs)
 
 def main():
     logger.info("Starting")
-    sonarr_host = get_config('SONAR_HOST')
-    sonarr_apikey = get_config('SONAR_APIKEY')
-    response = get_request(f'{sonarr_host}/api/queue?apikey={sonarr_apikey}')
+    sonarr_host = get_config('SONARR_HOST')
+    sonarr_apikey = get_config('SONARR_APIKEY')
+    check_sonarr_queue_for_unrar(sonarr_apikey, sonarr_host)
+    radarr_host = get_config('RADARR_HOST')
+    radarr_apikey = get_config('RADARR_APIKEY')
+    check_radarr_queue_for_unrar(radarr_apikey, radarr_host)
+    logger.info("Done")
+
+
+def check_sonarr_queue_for_unrar(apikey, host):
+    logger.info(f'Checking {host}')
+    response = get_request(f'{host}/api/queue?apikey={apikey}')
     response_json = response.json()
+    logger.debug(response_json)
     for item in response_json:
         status_messages = item['statusMessages']
         for status in status_messages:
@@ -24,7 +34,20 @@ def main():
                     unrar_files(item)
             if "One or more episodes expected in this release were not imported or missing" in status['title']:
                 unrar_files(item)
-    logger.info("Done")
+
+
+def check_radarr_queue_for_unrar(apikey, host):
+    logger.info(f'Checking {host}')
+    response = get_request(f'{host}/api/v3/queue', {'X-Api-Key': apikey})
+    response_json = response.json()
+    for item in response_json['records']:
+        status_messages = item['statusMessages']
+        for status in status_messages:
+            for message in status['messages']:
+                if "No files found are eligible for import" in message or "Sample" in message:
+                    unrar_files(item)
+            if "One or more episodes expected in this release were not imported or missing" in status['title']:
+                unrar_files(item)
 
 
 def unrar_files(item):
@@ -38,8 +61,8 @@ def unrar_files(item):
         subprocess.run(command, check=True)
 
 
-def get_request(url):
-    r = requests.get(url=url)
+def get_request(url, headers={}):
+    r = requests.get(url=url, headers=headers)
     return r
 
 
